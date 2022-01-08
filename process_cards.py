93061750f36fefd4a1fd5c364ea5fbbb3e329ecd
Christopher D. Chapman <chris@m3mnoch.com>
2022-01-08 10:24:35 -0800

    partners and multiple vices and phobias

--- process_cards.py
+++ process_cards.py
@@ -6,6 +6,7 @@
 import os
 from sys import version_info
 from PIL import Image, ImageDraw, ImageFont
+import textwrap
 import json
 import csv
 
@@ -18,12 +19,20 @@ weird_names = ["Aristotle", "Armpit", "Backsplash", "Bastardly", "Beaverclown", 
                "Market", "Marmaduke", "Marmalade", "Mercutio", "Moon", "Moreplease", "Mountainous", "Newcarsmell", "Nincompoop", "Noddy", "O'Yacklebrunt", "Oldmanbutt", "Outstanding", "Oysterbury", "Periscope", "Philtrum", "Pigpen", "Piles", "Pooney", "Prunella", "Rastafloods", "Ribeye", "Scrawnie", "Shivers", "Shrimpgun", "Sialoquent", "Silverback", "Sir", "Skyscraper", "Snuggles", "So-sure", "Sod", "Spike", "Stumble", "Sweet Tea", "Teeth", "Tidy", "Tiger", "Tweetly", "Van Clamberwobble", "Van Winkle", "Vanilla", "Wabbit", "Whatnot", "Widdershins", "Wigglebottom", "Woodstaff", "Zeus", "Zoro"]
 unique_names = ["Will Barrow", "Neil Down", "Danny Dan-Dandy", "Flick Balls", "Anil Probes", "Bon Jontovi", "Shania Grain", "Bunny Bundtcake", "Inspector Ratchet", "Paula Logan", "Indigo Foreverago", "Crackle Poppins", "Mary Kate", "Babe Truth", "Sue Ndamukong", "Wally World",
                 "Rustie Wingnut", "Crescent Moon", "Lily Liliac", "Constance Sufferman", "Vincent Van No", "Cobbed Corn", "Hugh Jackedman", "Ty Slob", "Kat Inmouse", "Kid Rockins", "Entire Pizza", "Glorious Esteban", "Leroy Jenkins", "Gray Matter", "Anita Hit", "Miranda Wright", "Goodwill Hunting"]
+partner_names = ["Lazy Lions", "Cryptodads", "Knights of Degen", "SoRare", "8 Bit Sports", "Footium", "Fives", "NFTeams", "Aifa",
+                 "Metasoccer", "NFT FC", "Krause House", "League DAO", "Ballerz", "All Star Freaks", "Big Head Soccer Club", "NFTUT", "Rookies"]
+
+
 overlayAnchor = (200, 100)
 
 max_cards = 5
 
 headerStyle = ImageFont.truetype(
     card_template_directory + "/Inter-Bold.ttf", 64)
+headerStyleSlim = ImageFont.truetype(
+    card_template_directory + "/Inter-Bold.ttf", 52)
+headerStyleSuperSlim = ImageFont.truetype(
+    card_template_directory + "/Inter-Bold.ttf", 36)
 statStyle = ImageFont.truetype(
     card_template_directory + "/Inter-Bold.ttf", 18)
 traitStyle = ImageFont.truetype(
@@ -57,6 +66,26 @@ def create_cards():
             break
 
     print("")
+    print("===> Finished.")
+    print("")
+
+
+def create_celebrity_cards():
+    print("============ PROCESSING CELEBRITY CARDS ============")
+    print("")
+
+    for filename in os.listdir(metadata_directory):
+        if filename.endswith('.json'):
+
+            with open(os.path.join(metadata_directory, filename)) as file:
+                jsonString = file.read()
+                index = filename.split(".")[0]
+                card_data = json.loads(jsonString)
+
+                if card_data["name"] in partner_names:
+                    print("creating card: " + str(index))
+                    merge_metadata(card_data, index)
+
     print("===> Finished.")
     print("")
 
@@ -124,6 +153,10 @@ def create_interesting_cards():
                         do_render = True
                         unique_count = unique_count + 1
 
+                # manual adds
+                if index in ["1120", "2376"]:
+                    do_render = True
+
                 if do_render:
                     merge_metadata(card_data, index)
 
@@ -139,7 +172,7 @@ def export_spreadsheet():
             sploot_characters, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         character_writer.writerow(['Name', 'Speed', 'Stamina', 'Strength', 'Aggression', 'Creativity', 'Luck',
-                                   'Focus', 'Influence', 'Agility', 'Phobia', 'Vice', 'Role', 'Personality', 'Class', 'Affinity'])
+                                   'Focus', 'Influence', 'Reflexes', 'Phobia', 'Vice', 'Role', 'Personality', 'Class', 'Affinity'])
 
         for filename in os.listdir(metadata_directory):
             if filename.endswith('.json'):
@@ -159,7 +192,7 @@ def export_spreadsheet():
                 character_row.append(get_attribute(card_data, "Luck"))
                 character_row.append(get_attribute(card_data, "Focus"))
                 character_row.append(get_attribute(card_data, "Influence"))
-                character_row.append(get_attribute(card_data, "Agility"))
+                character_row.append(get_attribute(card_data, "Reflexes"))
                 character_row.append(get_attribute(card_data, "Phobia"))
                 character_row.append(get_attribute(card_data, "Vice"))
                 character_row.append(get_attribute(card_data, "Role"))
@@ -185,6 +218,21 @@ def print_stats():
     management_count = 0
 
     traits = {}
+    stats = {
+        'mind': [],
+        'body': [],
+        'soul': [],
+        'ovr': [],
+        'gray': 0,
+        'blue': 0,
+        'purple': 0,
+        'gold': 0,
+    }
+    for i in range(100):
+        stats['mind'].append(0)
+        stats['body'].append(0)
+        stats['soul'].append(0)
+        stats['ovr'].append(0)
 
     for filename in os.listdir(metadata_directory):
         if filename.endswith('.json'):
@@ -236,6 +284,25 @@ def print_stats():
                     if unique_name in card_data["name"]:
                         unique_count = unique_count + 1
 
+                dna_avgs = get_dna_avgs(card_data["dna"])
+                stats["body"][dna_avgs['body']
+                              ] = stats["body"][dna_avgs['body']] + 1
+                stats["soul"][dna_avgs['soul']
+                              ] = stats["soul"][dna_avgs['soul']] + 1
+                stats["mind"][dna_avgs['mind']
+                              ] = stats["body"][dna_avgs['mind']] + 1
+                stats["ovr"][dna_avgs['ovr']
+                             ] = stats["body"][dna_avgs['ovr']] + 1
+
+                stats[get_color(dna_avgs)] = stats[get_color(dna_avgs)] + 1
+
+                if get_color(dna_avgs) == 'gold':
+                    print(json.dumps(card_data, indent=4))
+
+    print("dna average:")
+    print(json.dumps(stats, indent=4))
+    print("")
+
     print("___________CHARACTER_DISTRIBUTION____________")
     print("Unique: " + str(unique_count))
     print("Rares: " + str(rare_count))
@@ -244,12 +311,12 @@ def print_stats():
     print("Unlucky: " + str(harsh_count))
     print("")
 
-    print("___________ATTRIBUTE_DISTRIBUTION____________")
-    for trait_name in traits.keys():
-        print("")
-        print(trait_name + ":")
-        print(json.dumps(
-            dict(sorted(traits[trait_name].items(), key=lambda item: item[1])), indent=4))
+    # print("___________ATTRIBUTE_DISTRIBUTION____________")
+    # for trait_name in traits.keys():
+    #     print("")
+    #     print(trait_name + ":")
+    #     print(json.dumps(
+    #         dict(sorted(traits[trait_name].items(), key=lambda item: item[1])), indent=4))
 
     print("")
     print("===> Finished.")
@@ -263,6 +330,42 @@ def get_attribute(metadata, attr):
     return ""
 
 
+def get_dna_avgs(dna_list):
+    avgs = {}
+
+    avgs['body'] = int(round((dna_list[0]['code'] % 1 + dna_list[1]
+                       ['code'] % 1 + dna_list[2]['code'] % 1)/3, 2) * 100)
+    avgs['soul'] = int(round((dna_list[3]['code'] % 1 + dna_list[4]
+                       ['code'] % 1 + dna_list[5]['code'] % 1)/3, 2) * 100)
+    avgs['mind'] = int(round((dna_list[6]['code'] % 1 + dna_list[7]
+                       ['code'] % 1 + dna_list[8]['code'] % 1)/3, 2) * 100)
+    avgs['ovr'] = int(round((avgs['body'] + avgs['soul'] + avgs['mind'])/3, 2))
+
+    return avgs
+
+
+def get_color(avgs):
+    talent_count = 0
+    if avgs['body'] > 77:
+        talent_count = talent_count + 1
+
+    if avgs['soul'] > 77:
+        talent_count = talent_count + 1
+
+    if avgs['mind'] > 77:
+        talent_count = talent_count + 1
+
+    my_color = 'gray'
+    if talent_count == 3:
+        my_color = 'gold'
+    elif talent_count == 2:
+        my_color = 'purple'
+    elif talent_count == 1:
+        my_color = 'blue'
+
+    return my_color
+
+
 def merge_metadata(metadata, index):
 
     card_output_filename = card_output_directory + "/" + index + ".png"
@@ -270,7 +373,22 @@ def merge_metadata(metadata, index):
     if not os.path.exists(card_output_directory):
         os.makedirs(card_output_directory)
 
-    background = Image.open(card_template_directory + "/SPLOOT_Card_Grey.png")
+    my_avgs = get_dna_avgs(metadata["dna"])
+    my_color = get_color(my_avgs)
+
+    if my_color == 'gold':
+        background = Image.open(
+            card_template_directory + "/SPLOOT_Card_Gold.png")
+    elif my_color == 'purple':
+        background = Image.open(
+            card_template_directory + "/SPLOOT_Card_Pink.png")
+    elif my_color == 'blue':
+        background = Image.open(
+            card_template_directory + "/SPLOOT_Card_Blue.png")
+    else:
+        background = Image.open(
+            card_template_directory + "/SPLOOT_Card_Grey.png")
+
     width, height = background.size
 
     draw_target = ImageDraw.Draw(background)
@@ -313,23 +431,22 @@ def merge_metadata(metadata, index):
 
 def draw_name(draw_target, name, image_width):
 
-    # first name
-    first_name = name.split()[0].upper().strip()
-    textwidth, textheight = draw_target.textsize(
-        first_name, font=headerStyle)
-    x = image_width / 2 - textwidth / 2
-    y = 170 + overlayAnchor[1]
-    draw_target.text((x, y), first_name,
-                     (255, 255, 255), font=headerStyle)
+    wrapped = textwrap.wrap(name, width=12, break_long_words=False)
 
-    # last name
-    last_name = name.upper().replace(first_name, "", 1).strip()
-    textwidth, textheight = draw_target.textsize(
-        last_name, font=headerStyle)
-    x = image_width / 2 - textwidth / 2
-    y = 235 + overlayAnchor[1]
-    draw_target.text((x, y), last_name,
-                     (255, 255, 255), font=headerStyle)
+    lineHeight = 60
+    totalHeight = 60 * len(wrapped)
+    currentHeight = 210 + overlayAnchor[1] - totalHeight / 2
+
+    for line in wrapped:
+        w, h = draw_target.textsize(line, font=headerStyle)
+        textwidth, textheight = draw_target.textsize(
+            line, font=headerStyle)
+        x = image_width / 2 - textwidth / 2
+        y = currentHeight
+
+        draw_target.text((x, y), line,
+                         (255, 255, 255), font=headerStyle)
+        currentHeight += lineHeight
 
     return draw_target
 
@@ -382,9 +499,9 @@ def draw_phobia(draw_target, phobia, image_width):
     return draw_target
 
 
-def draw_class(draw_target, player_class, image_width):
+def draw_role(draw_target, role, image_width):
 
-    draw_string = player_class.upper()
+    draw_string = role.upper()
     textwidth, textheight = draw_target.textsize(
         draw_string, font=labelStyle)
 
@@ -397,9 +514,9 @@ def draw_class(draw_target, player_class, image_width):
     return draw_target
 
 
-def draw_role(draw_target, role, image_width):
+def draw_class(draw_target, char_class, image_width):
 
-    draw_string = role.upper()
+    draw_string = char_class.upper()
 
     textwidth, textheight = draw_target.textsize(
         draw_string, font=traitStyle)
@@ -417,8 +534,8 @@ def draw_stats(draw_target, attributes, image_width):
 
     stat_count = 0
     col_count = 0
-    col_gap = 165
-    row_gap = 36
+    col_gap = 170
+    row_gap = 35
     overall = 0.0
     running_total = 0.0
 
@@ -428,8 +545,8 @@ def draw_stats(draw_target, attributes, image_width):
             running_total = running_total + attribute_data["value"]
             overall = overall + attribute_data["value"]
 
-            x = overlayAnchor[0] + (col_count * col_gap) + 210
-            y = overlayAnchor[1] + (stat_count % 3 * row_gap) + 600
+            x = overlayAnchor[0] + (col_count * col_gap) + 200
+            y = overlayAnchor[1] + (stat_count % 3 * row_gap) + 625
             draw_target.text((x, y), str(attribute_data["value"]),
                              (255, 255, 255), font=statStyle)
 
@@ -438,8 +555,8 @@ def draw_stats(draw_target, attributes, image_width):
             if stat_count % 3 == 0:
                 # draw col average.
                 x = overlayAnchor[0] + \
-                    (col_count * col_gap) + 203
-                y = overlayAnchor[1] + (3 * row_gap) + 600
+                    (col_count * col_gap) + 200
+                y = overlayAnchor[1] + (3 * row_gap) + 625
                 draw_target.text((x, y), str(round(running_total/3, 1)),
                                  (196, 196, 196), font=statStyle)
 
@@ -447,8 +564,8 @@ def draw_stats(draw_target, attributes, image_width):
                 col_count = col_count + 1
 
     # overall
-    x = 105 + overlayAnchor[0]
-    y = 420 + overlayAnchor[1]
+    x = 111 + overlayAnchor[0]
+    y = 458 + overlayAnchor[1]
     draw_target.text((x, y), str(round(overall/9, 1)),
                      (255, 255, 255), font=ovrStyle)
 
@@ -460,7 +577,7 @@ def draw_dna_band(draw_target, dna, image_width):
     segment_width = 50
     segment_height = 3
     x_anchor = image_width / 2 - segment_width * 4.5
-    y_anchor = 510 + overlayAnchor[1]
+    y_anchor = 545 + overlayAnchor[1]
     dna_counter = 0
 
     # draw the gray base.
@@ -500,6 +617,7 @@ def main_menu():
     print("========= MAIN MENU ===========")
     print("a) Create All Cards")
     print("i) Create Interesting Cards")
+    print("c) Create Celebrity Cards")
     print("p) Print Stats")
     print("s) Character Spreadsheet")
     print("-------------------")
@@ -514,6 +632,10 @@ def main_menu():
 
     if menuSelection.lower() == "i":
         create_interesting_cards()
+        main_menu()
+
+    if menuSelection.lower() == "c":
+        create_celebrity_cards()
         main_menu()
 
     elif menuSelection.lower() == "p":
